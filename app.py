07f89bcb0a3d4fd01e16951fb9f13b140358bcb5
Блажиевская Александра <blazhievskaya_aleksandra@MacBook-Air-Blazievskaa.local>
2015-05-27 17:41:14 +0300

    changed lines

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, send_from_directory,redirect, url_for, render_template
+﻿from flask import Flask, request, send_from_directory,redirect, url_for, render_template
 import codecs, os, pprint
 from xml.dom.minidom import parse, parseString
 from werkzeug import secure_filename
